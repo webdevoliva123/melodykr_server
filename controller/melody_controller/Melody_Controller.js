959100c844f057ee3349ccd9d20d4cecb613572a
abhishek <98287339+WebDevOliva@users.noreply.github.com>
2024-01-07 03:25:09 +0530

    2 more response added

--- controller/melody_controller/Melody_Controller.js
+++ controller/melody_controller/Melody_Controller.js
@@ -2,8 +2,8 @@ const ARTICLE = require("../../models/article_model/Article");
 
 exports.getMelodyHomeData = async (req,res) => {
     try {
-        const fieldsToRetrieve = 'title thumbnail createdAt';
-        const articleHeadline = await ARTICLE.find({isHeadline:true}).select(fieldsToRetrieve).limit(10)
+        const fieldsToRetrieve = 'title thumbnail category createdAt';
+        const articleHeadline = await ARTICLE.find({isHeadline:true}).select(fieldsToRetrieve).sort({ createdAt: 1 }).limit(10)
         return res.status(200).json({
             success : true,
             message: 'Home Data Loaded',
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`);
+app.listen(5500, () => {
+  console.log(`Example app listening on port ${5500}`);
 });
