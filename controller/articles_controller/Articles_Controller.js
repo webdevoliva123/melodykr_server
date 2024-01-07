cbdddb1396be13b9835a533f592dca89d82edc74
abhishek <98287339+WebDevOliva@users.noreply.github.com>
2024-01-07 18:52:00 +0530

    get article by id api created

--- controller/articles_controller/Articles_Controller.js
+++ controller/articles_controller/Articles_Controller.js
@@ -7,46 +7,61 @@ exports.getArticle = async (req, res) => {
 
     // article info
     const findArticle = await ARTICLE.findById(article_id);
-    const { author, ...otherData } = findArticle?._doc;
+    
+    if(findArticle){
+       const { author, ...otherData } = findArticle?._doc;
 
-    // article author info
-    const authordata = "_id socialMedia profileImage name email bio";
-    const findAuthor = await CONTENT_WRITER?.findById(author?._id)?.select(
-      authordata
-    );
+     // article author info
+     const authordata = "_id socialMedia profileImage name email bio";
+     const findAuthor = await CONTENT_WRITER?.findById(author?._id)?.select(
+       authordata
+     );
+ 
+     // upcomming article
+     const upcommingarticledata = "_id thumbnail title category createdAt";
+     const findNextArticle = await ARTICLE?.find({
+       createdAt: { $gt: otherData?.createdAt },
+     })
+       .select(upcommingarticledata)
+       .limit(1);
+     const findPrevArticle = await ARTICLE?.find({
+       createdAt: { $lt: otherData?.createdAt },
+     })
+       .select(upcommingarticledata)
+       .limit(1);
+     const findRelatedArticle = await ARTICLE?.find({
+       category: otherData?.category,
+     })
+       .select(upcommingarticledata)
+       .sort({ createdAt: -1 })
+       .limit(3);
+ 
+     return res.status(200).json({
+       success: true,
+       message: "Article has been found!",
+       data: {
+         ...otherData,
+         author: findAuthor,
+         upcomming_article: {
+           next_article: findNextArticle,
+           prev_article: findPrevArticle,
+         },
+         related_article: findRelatedArticle,
+       },
+     });
+   }else{
+    const latest_article = "_id thumbnail title category createdAt";
+    const findlatestArticle = await ARTICLE?.find()
+        .select(latest_article)
+        .sort({ createdAt: -1 })
+        .limit(10);
+    return res.status(404).json({
+        sucess : false,
+        error : "We couldn't find any articles by this id. Don't worry, Explore latest articles.",
+        data : findlatestArticle
 
-    // upcomming article
-    const upcommingarticledata = "_id thumbnail title category createdAt";
-    const findNextArticle = await ARTICLE?.find({
-      createdAt: { $gt: otherData?.createdAt },
     })
-      .select(upcommingarticledata)
-      .limit(1);
-    const findPrevArticle = await ARTICLE?.find({
-      createdAt: { $lt: otherData?.createdAt },
-    })
-      .select(upcommingarticledata)
-      .limit(1);
-    const findRelatedArticle = await ARTICLE?.find({
-      category: otherData?.category,
-    })
-      .select(upcommingarticledata)
-      .sort({ createdAt: -1 })
-      .limit(3);
-
-    return res.status(200).json({
-      success: true,
-      message: "",
-      data: {
-        ...otherData,
-        author: findAuthor,
-        upcomming_article: {
-          next_article: findNextArticle,
-          prev_article: findPrevArticle,
-        },
-        related_article: findRelatedArticle,
-      },
-    });
+   }
   } catch (error) {
     return res.status(500).json({
       success: false,
